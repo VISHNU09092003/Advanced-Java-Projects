526c6fbf65c8a5da10d2b2f60b842420bd529186
VISHNU VARDHAN REDDY <vishnupeddireddy0188@gmail.com>
2023-02-27 21:24:45 +0530

    Create Test7.java
    completed

---
+++ Test7.java
@@ -0,0 +1,19 @@
+//prrogramm on checked exception (serious)
+//ex (FileNotFoundException, InterruptedException, SQLEXcpeption)
+//for checked exception try-catch is madatory)
+//throws i an
+//ignoring excpeyion
+public class Test7 {
+
+	public static void main(String[] args) throws Exception {
+		
+	System.out.println("Programm  begins");
+	//throw new ArithmetricException("Divisionb by zero");
+	throw new InterruptedException("Thread Interruption");
+	
+	
+
+
+	}
+
+}
