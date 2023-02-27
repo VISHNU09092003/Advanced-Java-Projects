3d151d7f61fffc7f090246586aad3fc2407a5fe9
VISHNU VARDHAN REDDY <vishnupeddireddy0188@gmail.com>
2023-02-27 21:24:11 +0530

    Create Test6.java
    completed

---
+++ Test6.java
@@ -0,0 +1,16 @@
+//programm on unchecked exception (not serius)
+//ex (ArthimeticException, nullpointer,Arrayindexouyofbounds)
+public class Test6 {
+	public static void main(String [] args) {
+	System.out.println("proogramm begins");
+	//throw new ArithmeticException("problem with division");
+	//System.out.println("end of the programm");
+	int x=6;
+	int y=0;
+	int z=x/y;    //run time mistake
+	System.out.println("end of the programm");
+	
+		
+	}
+
+}
